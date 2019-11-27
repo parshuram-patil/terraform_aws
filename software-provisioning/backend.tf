43e9c03b1df3323ecb14a8a84dd2be36ed074e09
parashuram-patil <parasharam_patil@ymail.com>
2019-11-27 12:01:34 +0530

    Code formatting : terraform fmt
    -- formats the code

--- software-provisioning/backend.tf
+++ software-provisioning/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "psp-terraform-states"
-    key = "terraform/first"
+    bucket  = "psp-terraform-states"
+    key     = "terraform/first"
     profile = "terraform"
   }
 }
