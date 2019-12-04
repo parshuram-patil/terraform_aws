6117ded353a2aa1750cc5c50ced45462ed9f061f
parashuram-patil <parasharam_patil@ymail.com>
2019-12-04 16:26:18 +0530

    Creating EC2 instance in public subnet with security group

--- vpc/vars.tf
+++ vpc/vars.tf
@@ -2,11 +2,20 @@ variable "AWS_REGION" {
   default = "eu-west-1"
 }
 
-/*variable "PATH_TO_PRIVATE_KEY" {
+variable "PATH_TO_PRIVATE_KEY" {
   default = "mykey"
 }
 
 variable "PATH_TO_PUBLIC_KEY" {
   default = "mykey.pub"
-}*/
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-00068cd7555f543d5"
+    us-west-1 = "ami-0b2d8d1abb76a53d8"
+    eu-west-1 = "ami-01f14919ba412de34"
+  }
+}
 
