96ccae4b1cb529558210a2466c519cd0a016940b
parashuram-patil <parasharam_patil@ymail.com>
2020-01-16 13:11:06 +0530

    Refactoring

--- lambda/api_gateway.tf
+++ lambda/api_gateway.tf
@@ -8,7 +8,7 @@ resource "aws_api_gateway_rest_api" "example" {
 resource "aws_api_gateway_resource" "first" {
   rest_api_id = aws_api_gateway_rest_api.example.id
   parent_id   = aws_api_gateway_rest_api.example.root_resource_id
-  path_part   = "user"
+  path_part   = "first"
 }
 
 resource "aws_api_gateway_method" "first_get" {
