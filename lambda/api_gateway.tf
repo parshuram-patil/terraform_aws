a17b41382026dc9428c9ffce02c105774f3c42a5
parashuram-patil <parasharam_patil@ymail.com>
2020-01-16 14:45:01 +0530

    Nested resources API gateway

--- lambda/api_gateway.tf
+++ lambda/api_gateway.tf
@@ -67,4 +67,59 @@ resource "aws_api_gateway_integration" "second" {
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.second_lambda.invoke_arn
 }
+
+# Everytime you add new resource it has to deployed to new/existing stage
+# if we do it from UI and plan it there will be no change
+resource "aws_api_gateway_deployment" "second_deployment" {
+  depends_on = [
+    aws_api_gateway_integration.second
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  stage_name  = "test"
+}
+
+output "second_deployment" {
+  value = aws_api_gateway_deployment.second_deployment.invoke_url
+}
+#------------------------------------------------------------------------------------------
+
+
+#------------------------------- First Nested lambda --------------------------------------------
+
+resource "aws_api_gateway_resource" "first_nested" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  parent_id   = aws_api_gateway_resource.first.id
+  path_part   = "nested"
+}
+
+resource "aws_api_gateway_method" "first_nested_get" {
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+  resource_id   = aws_api_gateway_resource.first_nested.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "first_nested" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_method.first_nested_get.resource_id
+  http_method = aws_api_gateway_method.first_nested_get.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.first_nested_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "first_nested_deployment" {
+  depends_on = [
+    aws_api_gateway_integration.first_nested
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  stage_name  = "test"
+}
+
+output "first_nested_deployment" {
+  value = aws_api_gateway_deployment.first_nested_deployment.invoke_url
+}
 #------------------------------------------------------------------------------------------
--- lambda/lambda.tf
+++ lambda/lambda.tf
@@ -1,6 +1,7 @@
 locals {
   first_zip_location = "output/first.zip"
   second_zip_location = "output/second.zip"
+  first_nested_zip_location = "output/first_nested.zip"
 }
 
 #------------------------------- First lambda --------------------------------------------
@@ -57,6 +58,36 @@ resource "aws_lambda_permission" "second_lambda" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.second_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/*/*"
+}
+#------------------------------------------------------------------------------------------
+
+
+#------------------------------- First Nested lambda --------------------------------------------
+
+data "archive_file" "first_nested_archive" {
+  type        = "zip"
+  source_file = "code/first_nested.py"
+  output_path = local.first_nested_zip_location
+}
+
+resource "aws_lambda_function" "first_nested_lambda" {
+  filename      = local.first_nested_zip_location
+  function_name = "first_nested_lambda"
+  role          = aws_iam_role.psp_lambda_role.arn
+  handler       = "first_nested.hello"
+  memory_size   = 250
+
+  source_code_hash = filebase64sha256(data.archive_file.first_nested_archive.output_path)
+
+  runtime = "python3.7"
+}
+
+resource "aws_lambda_permission" "first_nested_lambda" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.first_nested_lambda.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/*/*"
 }
