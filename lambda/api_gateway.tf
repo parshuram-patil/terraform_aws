4e7eeb7a43d89eb3d4b8fa82fa5028a1f52c9c7f
parashuram-patil <parasharam_patil@ymail.com>
2020-01-16 15:09:29 +0530

    {proxy+} impl

--- lambda/api_gateway.tf
+++ lambda/api_gateway.tf
@@ -95,17 +95,23 @@ resource "aws_api_gateway_resource" "first_nested" {
   path_part   = "nested"
 }
 
-resource "aws_api_gateway_method" "first_nested_get" {
+resource "aws_api_gateway_resource" "first_nested_proxy" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  parent_id   = aws_api_gateway_resource.first_nested.id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "first_nested_proxy" {
   rest_api_id   = aws_api_gateway_rest_api.example.id
-  resource_id   = aws_api_gateway_resource.first_nested.id
-  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.first_nested_proxy.id
+  http_method   = "ANY"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "first_nested" {
+resource "aws_api_gateway_integration" "first_nested_proxy" {
   rest_api_id = aws_api_gateway_rest_api.example.id
-  resource_id = aws_api_gateway_method.first_nested_get.resource_id
-  http_method = aws_api_gateway_method.first_nested_get.http_method
+  resource_id = aws_api_gateway_method.first_nested_proxy.resource_id
+  http_method = aws_api_gateway_method.first_nested_proxy.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -114,7 +120,7 @@ resource "aws_api_gateway_integration" "first_nested" {
 
 resource "aws_api_gateway_deployment" "first_nested_deployment" {
   depends_on = [
-    aws_api_gateway_integration.first_nested
+    aws_api_gateway_integration.first_nested_proxy
   ]
 
   rest_api_id = aws_api_gateway_rest_api.example.id
