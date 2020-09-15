937d8a7d11dfc5fbec5d848a23c5a6377414102a
parshuram-patil <parshuram.patil@outlook.in>
2020-09-15 15:10:09 +0530

    CORS script update

--- cors/code/first.py
+++ cors/code/first.py
@@ -1,7 +1,12 @@
-def hello(event, context):
-    message = ["Welcome", "To", "First", "Python", "Lambda", "Function"]
-    print(message)
+import time
 
+def hello(event, context):
+    print('Execution Started')
+    # raise Exception('My Exception!')
+    time.sleep(5000)
+    message = ["Welcome", "To", "Python", "Lambda", "Function"]
+    print(message)
+    print('Execution Ended')
     # Body and Status Code is mandatory if you are using lambda with API gateway
     return {
         "body": message,
